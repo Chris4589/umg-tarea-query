3b21d39928c027be881470537fa4aca57a8cc2af
Christopher Herrera <cherrera@is4tech.com>
2024-09-16 19:23:12 -0600

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import org.example.repository.PersonaRepository;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         try {
             var connection = MysqlConfig.getConnection();
             System.out.println("Connection established!");
